63823c9ee3dcc12a0436f6cca1e665a304b3b833
Sherly789 <sherlysh789>
2016-06-24 10:27:42 -0700

    count numbrt from one to the number

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
+// Business logic
+function countNumber(number) {
+  var newInput = [];
+  for ( var i = 1; i <= number; i += 1 ) {
+    newInput.push(i);
+  }
+
+  return newInput;
+};
+
+
+
+
+
+
+
+
+
 // user interface
   $(document).ready(function() {
     $("form#ping-pong").submit(function(event) {
       event.preventDefault();
 
       var number = parseInt($("input#input").val());
+      var Output = countNumber(number)
+
+      $("#result").text(Output);
+    });
+  });
